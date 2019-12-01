cf139a2cb3e12fefdcf2e5bcc1e8d93ba589b86d
Aaron <43405823+HTMLAaron@users.noreply.github.com>
2019-12-01 14:48:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const ObjectId = require('mongodb').ObjectID;
 const mongoDBurl = 'mongodb+srv://aaron:aaronso@aarondb-ep2mi.mongodb.net/test?retryWrites=true&w=majority';
-const dbName = 's381aasignment';
+const dbName = 's381assignment';
 var session = require('cookie-session');
 var express = require('express');
 
